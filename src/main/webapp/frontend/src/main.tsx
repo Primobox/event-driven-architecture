1a5162029de4c4c49741d99cbee3e0389253a181
Vincent Dubois <v.dubois@primobox.com>
2022-09-17 20:52:48 +0200

    Adresse du backend en HTTPS

--- src/main/webapp/frontend/src/main.tsx
+++ src/main/webapp/frontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-export const BASE_API_URL = 'http://eventdriven.ddns.net:8080';
+export const BASE_API_URL = 'https://eventdriven.ddns.net';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
