1dfe434f3b6f57fc301e54000d113d3a544c318e
Vincent Dubois <v.dubois@primobox.com>
2022-09-17 20:48:49 +0200

    Backend en HTTPS

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 git pull
 docker build -t vdubois/primobox-event-driven-architecture:latest .
 docker rm -v -f event-driven-architecture | true
-docker run -d --name event-driven-architecture -p 8080:8080 -e SPRING_SECURITY_USER_NAME=$SPRING_SECURITY_USER_NAME -e SPRING_SECURITY_USER_PASSWORD=$SPRING_SECURITY_USER_PASSWORD vdubois/primobox-event-driven-architecture:latest
+docker run -d --name event-driven-architecture -p 443:8443 -e SPRING_SECURITY_USER_NAME=$SPRING_SECURITY_USER_NAME -e SPRING_SECURITY_USER_PASSWORD=$SPRING_SECURITY_USER_PASSWORD vdubois/primobox-event-driven-architecture:latest
